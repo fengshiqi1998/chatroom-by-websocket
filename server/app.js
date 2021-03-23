be88e36f2b9c3f66be3089352ae41daf282dc258
fengshiqi1998 <512653808@qq.com>
2021-03-23 15:18:35 +0800

    feat(log):添加log

--- server/app.js
+++ server/app.js
@@ -2,44 +2,40 @@ const WebSocket = require('ws');
 
 const server = new WebSocket.Server({ port: 3100 });
 
-const roomList = [];
-// let roomId = 0;
+const roomList = []; // 房间列表
 
 server.on('connection', (ws, req) => {
   const ip = req.connection.remoteAddress;
   const port = req.connection.remotePort;
   ws.on('message', (msg) => {
+    console.log(
+      'date: ',
+      new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    );
+    console.log('ws message before:\n', roomList);
     console.log('ws message: ', msg);
-    console.log(roomList);
-    // if (roomList.length === 0) {
-    //   roomList.push();
-    // }
-    // if (roomList.length === 0 || index === -1) {
-    // }
-    let str = '';
+    let str = ''; // 要发送的信息
     try {
       const data = JSON.parse(msg);
-
       const client = data.username;
       let curRoom = null;
+      // 若客户端为 mac
       if (client.indexOf('mac') > -1) {
         let hasMac = -1;
         let hasSeat = -1;
+        // 查询是否有该 mac 的记录
         hasMac = roomList.findIndex(
           (item) => item.mac && item.mac.name === client
         );
+        // 查询是否有空余的 mac 位置
         hasSeat = roomList.findIndex((item) => !item.mac);
-        // if (roomList.length === 0 || hasSeat === -1) {
-        //   const room = {
-        //     mac: client,
-        //     phone: ''
-        //   };
-        //   roomList.push(room)
-        // }
         switch (true) {
+          // mac已存在于某一房间中，则直接拿出来
           case hasMac > -1:
             curRoom = roomList[hasMac];
+            curRoom.mac.socket = ws; // 更新socket
             break;
+          // 有空位则将 mac 放入该房间中
           case hasSeat > -1:
             roomList[hasSeat].mac = {
               name: client,
@@ -47,32 +43,25 @@ server.on('connection', (ws, req) => {
             };
             curRoom = roomList[hasSeat];
             break;
-          // case roomList.length > 0:
-          //   const room = {
-          //     mac: {
-          //       name: client,
-          //       socket: ws
-          //     },
-          //     phone: '',
-          //   };
-          //   roomList.push(room);
-          //   break;
+          // 其他情况，目前考虑有：没有多余的房间 ，则新建一个房间
           default:
             const room = {
               mac: {
                 name: client,
                 socket: ws,
               },
-              phone: '',
+              phone: {},
             };
             roomList.push(room);
             curRoom = room;
             break;
         }
+        // 若房间中有 phone 则对 phone 进行通信
         if (curRoom.phone && curRoom.phone.socket) {
           str = `${data.username} say: ${data.message}`;
           curRoom.phone.socket.send(str);
         }
+        // 相反的情况
       } else {
         let hasPhone = -1;
         let hasSeat = -1;
@@ -83,6 +72,7 @@ server.on('connection', (ws, req) => {
         switch (true) {
           case hasPhone > -1:
             curRoom = roomList[hasPhone];
+            curRoom.phone.socket = ws;
             break;
           case hasSeat > -1:
             roomList[hasSeat].phone = {
@@ -97,7 +87,7 @@ server.on('connection', (ws, req) => {
                 name: client,
                 socket: ws,
               },
-              phone: '',
+              mac: {},
             };
             roomList.push(room);
             curRoom = room;
@@ -111,7 +101,8 @@ server.on('connection', (ws, req) => {
     } catch (e) {
       str = msg;
     }
-
+    console.log('ws message after:\n', roomList, '\n');
+    // // 遍历所有的客户端
     // server.clients.forEach((client) => {
     //   if (client.readyState === WebSocket.OPEN) {
     //     client.send(str);
@@ -124,8 +115,8 @@ server.on('connection', (ws, req) => {
     ws.send('ws connected');
   });
 
-  ws.on('close', () => {
-    console.log('ws close');
+  ws.on('close', (e) => {
+    console.log('ws close', e);
     ws.send('ws disconnected');
   });
 });
